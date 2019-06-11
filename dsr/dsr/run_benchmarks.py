db445b1672056018d73b6938c828fa288dad00c1
Petersen, Brenden Kyle <petersen33@llnl.gov>
2019-06-10 18:05:11 -0700

    Added configurable script to run benchmarks with either DSR or GP.
    Improved GP test script to read benchmark from config.

--- dsr/dsr/run_benchmarks.py
+++ dsr/dsr/run_benchmarks.py
@@ -6,12 +6,13 @@ import click
 import pandas as pd
 from sympy.parsing.sympy_parser import parse_expr
 from sympy import srepr
+from gplearn.genetic import SymbolicRegressor
 
 from dsr.program import Program
 from dsr.dataset import Dataset
 
 
-def train_benchmark(name, config_dataset, config_controller, config_training):
+def train_dsr(name, config_dataset, config_controller, config_training):
 
     try:
         import tensorflow as tf
@@ -22,9 +23,7 @@ def train_benchmark(name, config_dataset, config_controller, config_training):
         pass
 
     # Create the dataset
-    config_dataset["name"] = name
-    dataset = Dataset(**config_dataset)
-    X, y = dataset.X_train, dataset.y_train
+    X, y = get_dataset(name, config_dataset)
 
     # Define the library
     Program.set_library(config_dataset["operators"], X.shape[1])
@@ -39,18 +38,87 @@ def train_benchmark(name, config_dataset, config_controller, config_training):
         # Instantiate the controller
         controller = Controller(sess, n_choices=n_choices, **config_controller)
 
+        # Train the controller
         result = learn(sess, controller, X, y, **config_training) # Reward, expression, traversal
         result["name"] = name
         return result
 
 
+def train_gp(name, config_dataset, config_gp):
+
+    # Create the dataset
+    X, y = get_dataset(name, config_dataset)
+
+    # Configure parameters
+    if "init_depth" in config_gp:
+        config_gp["init_depth"] = tuple(config_gp["init_depth"])
+    config_gp["verbose"] = 0 # Turn off printing
+
+    # Create the GP
+    gp = SymbolicRegressor(**config_gp)
+
+    # Fit the GP
+    gp.fit(X, y)
+
+    # Retrieve best results
+    r = gp._program.raw_fitness_
+    
+    # Currently outputting seralized program in place of its corresponding traversal
+    program = str(gp._program)
+
+    # Many failure cases right now for converting to SymPy expression...not high priority to fix
+        # To do: serialized program --> tree --> SymPy-compatible tree --> traversal --> SymPy expression
+    try:
+        expression = repr(parse_expr(str(gp._program).replace("X", "x").replace("add", "Add").replace("mul", "Mul")))
+    except:
+        expression = "N/A"
+
+    result = {
+            "name" : name,
+            "r" : r,            
+            "expression" : expression,
+            "traversal" : program
+    }
+    return result
+
+
+# Creates and returns the dataset
+def get_dataset(name, config_dataset):    
+    config_dataset["name"] = name
+    dataset = Dataset(**config_dataset)
+    X, y = dataset.X_train, dataset.y_train
+    return X, y
+
+
+# Function wrapper to protect against ValueErrors
+def protect_ValueError(name, function):
+    try:
+        result = function(name)
+    except ValueError:
+        print("Encounted ValueError for benchmark {}".format(name))
+        result = {
+            "name" : name,
+            "r" : "N/A",
+            "expression" : "N/A",
+            "traversal" : "N/A"
+        }
+
+    return result
+
+
 @click.command()
 @click.argument('config_filename', default="config.json")
-@click.option('--output_filename', default="benchmark_results.csv", help="Filename to write results")
+@click.option('--method', default="dsr", type=click.Choice(["dsr", "gp"]), help="Symbolic regression method")
+@click.option('--output_filename', default=None, help="Filename to write results")
 @click.option('--num_cores', default=multiprocessing.cpu_count(), help="Number of cores to use")
 @click.option('--exclude_fp_constants', is_flag=True, help="Exclude benchmark expressions containing floating point constants")
 @click.option('--exclude_int_constants', is_flag=True, help="Exclude benchmark expressions containing integer constants")
-def main(config_filename, output_filename, num_cores, exclude_fp_constants, exclude_int_constants):
+@click.option('--exclude', multiple=True, type=str, help="Exclude benchmark expressions containing these names")
+@click.option('--only', multiple=True, type=str, help="Only include benchmark expressions containing these names (overrides other exclusions)")
+def main(config_filename, method, output_filename, num_cores, exclude_fp_constants, exclude_int_constants, exclude, only):
+
+    if output_filename is None:
+        output_filename = "benchmark_{}.csv".format(method)
     
      # Load the config file
     with open(config_filename, encoding='utf-8') as f:
@@ -59,23 +127,38 @@ def main(config_filename, output_filename, num_cores, exclude_fp_constants, excl
     config_dataset = config["dataset"]          # Problem specification parameters
     config_training = config["training"]        # Training hyperparameters
     config_controller = config["controller"]    # Controller hyperparameters
+    config_gp = config["gp"]                    # GP hyperparameters
 
     # Load the benchmark names
     df = pd.read_csv(config_dataset["file"], encoding="ISO-8859-1")
     names = df["name"].to_list()
 
     # Filter out expressions
-    expressions = [parse_expr(e) for e in df["expression"]]
-    keep = [True]*len(expressions)
-    if exclude_fp_constants:
-        keep = [False if "Float" in srepr(e) else k for k,e in zip(keep, expressions)]
-    if exclude_int_constants:
-        keep = [False if "Integer" in srepr(e) else k for k,e in zip(keep, expressions)]
+    expressions = [parse_expr(e) for e in df["expression"]]    
+    if len(only) == 0:
+        keep = [True]*len(expressions)
+        if exclude_fp_constants:
+            keep = [False if "Float" in srepr(e) else k for k,e in zip(keep, expressions)]
+        if exclude_int_constants:
+            keep = [False if "Integer" in srepr(e) else k for k,e in zip(keep, expressions)]
+        for excluded_name in exclude:
+            keep = [False if excluded_name in n else k for k,n in zip(keep, names)]
+    else:
+        keep = [False]*len(expressions)
+        for included_name in only:
+            keep = [True if included_name in n else k for k,n in zip(keep, names)]
+
     names = [n for k,n in zip(keep, names) if k]
 
+    # Define the work
+    if method == "dsr":
+        work = partial(train_dsr, config_dataset=config_dataset, config_controller=config_controller, config_training=config_training)
+    elif method == "gp":
+        work = partial(train_gp, config_dataset=config_dataset, config_gp=config_gp)
+    work = partial(protect_ValueError, function=work)
+
     # Farm out the work
-    pool = multiprocessing.Pool(num_cores)
-    work = partial(train_benchmark, config_dataset=config_dataset, config_controller=config_controller, config_training=config_training)
+    pool = multiprocessing.Pool(num_cores)    
     columns = ["name", "r", "expression", "traversal"]
     pd.DataFrame(columns=columns).to_csv(output_filename, header=True, index=False)
     for result in pool.imap_unordered(work, names):
--- scripts/gplearn_test.py
+++ scripts/gplearn_test.py
@@ -1,40 +1,85 @@
+import json
+from textwrap import indent
+
 from gplearn.genetic import SymbolicRegressor
-import numpy as np
+from sympy.parsing.sympy_parser import parse_expr
+from sympy import pretty
 
+from dsr.dataset import Dataset
 
-# Generate a dataset
-seed = 0
-rng = np.random.RandomState(seed)
-X_train = rng.uniform(-5,5,1200).reshape(600,2)
+# # Generate a dataset
+# seed = 0
+# rng = np.random.RandomState(seed)
+# # X_train = rng.uniform(-5,5,1200).reshape(600,2)
+# X_train = rng.uniform(0.1,5.9,60).reshape(30,2)
 
-##### sin(x^2) + cos(x*y) + sin(y^2) #####
-# y_train = np.sin(X_train[:,0]**2) + np.cos(X_train[:,1]*X_train[:,0]) + np.sin(X_train[:,1]**2)
+# ##### sin(x^2) + cos(x*y) + sin(y^2) #####
+# # y_train = np.sin(X_train[:,0]**2) + np.cos(X_train[:,1]*X_train[:,0]) + np.sin(X_train[:,1]**2)
 
-##### sin(x^2) + cos(x*y)/sin(y^2) #####
-# y_train = np.sin(X_train[:,0]**2) + np.cos(X_train[:,1]*X_train[:,0]) / np.sin(X_train[:,1]**2)
+# ##### sin(x^2) + cos(x*y)/sin(y^2) #####
+# # y_train = np.sin(X_train[:,0]**2) + np.cos(X_train[:,1]*X_train[:,0]) / np.sin(X_train[:,1]**2)
 
-##### sin(x^2) + cos(x*y)*sin(y^2) #####
-y_train = np.sin(X_train[:,0]**2) + np.cos(X_train[:,1]*X_train[:,0]) * np.sin(X_train[:,1]**2)
+# ##### sin(x^2) + cos(x*y)*sin(y^2) #####
+# # y_train = np.sin(X_train[:,0]**2) + np.cos(X_train[:,1]*X_train[:,0]) * np.sin(X_train[:,1]**2)
 
-# Define functino set
-function_set = ('add','mul','sin','cos')
+# ##### sin(x^2) + cos(x*y)*sin(y^2) #####
+# # y_train = 6 * np.sin(X_train[:,0]) + np.cos(X_train[:,1])
+
+# Load the config file
+config_filename = 'config.json'
+with open(config_filename, encoding='utf-8') as f:
+    config = json.load(f)
+
+config_dataset = config["dataset"]
+config_gp = config["gp"]
+
+# Special case: 'init_depth' is a tuple but JSON can't make tuples
+if "init_depth" in config_gp:
+    config_gp["init_depth"] = tuple(config_gp["init_depth"])
+
+# Create the dataset
+dataset = Dataset(**config_dataset)
+X, y = dataset.X_train, dataset.y_train
+
+# Define function set
+function_set = config_dataset["operators"]
 
 # Create the symbolic regression model
-ga = SymbolicRegressor(
-    population_size=5000,
-    generations=20,
-    stopping_criteria=0.01,
-    p_crossover=0.7,
-    p_subtree_mutation=0.1,
-    p_hoist_mutation=0.05,
-    p_point_mutation=0.1,
-    max_samples=0.9,
-    verbose=1,
-    parsimony_coefficient=0.01,
-    random_state=0,
-    function_set=function_set)
+gp = SymbolicRegressor(**config_gp)
+# gp = SymbolicRegressor(
+#     population_size=1000,
+#     generations=1,
+#     tournament_size=20,
+#     stopping_criteria=0.0,
+#     const_range=None, # Changed from default
+#     init_depth=(2,6),
+#     init_method="half and half",
+#     function_set=function_set, # Changed from default
+#     metric="mse", # Changed from default
+#     parsimony_coefficient=0.001,
+#     p_crossover=0.9,
+#     p_subtree_mutation=0.01,
+#     p_hoist_mutation=0.01,
+#     p_point_mutation=0.01,
+#     p_point_replace=0.05,
+#     max_samples=1.0,
+#     feature_names=None,
+#     warm_start=False,
+#     low_memory=False,
+#     n_jobs=1,
+#     verbose=1, # Changed from default
+#     random_state=0 # Changed from default
+#     )
 
 # Fit using genetic algorithm
-ga.fit(X_train, y_train)
+gp.fit(X, y)
 
-print(ga._program)
+# Retrieve results
+print("Reward:", gp._program.raw_fitness_)
+print("Program:", str(gp._program))
+try: # Parsing serialized program does not always work
+    best_str = str(gp._program).replace("X", "x").replace("add", "Add").replace("mul", "Mul")
+    sympy_expr = parse_expr(best_str)
+    print("{}".format(indent(pretty(sympy_expr), 'Expression: ')))
+except:
+    pass
