dso-org/deep-symbolic-optimization

commit 5bf4baf0e92a5c82db860b8c8971d9020e5e6395

Constant optimizer always optimizes MSE, regardless of reward function. This required minor refactoring since constant optimizer no longer computes base_r.

dsr/dsr/program.py
@@ -142,9 +142,7 @@ class Program(object):
         self.traversal = [Program.library[t] for t in tokens]
         self.const_pos = [i for i,t in enumerate(tokens) if t == Program.const_token]
         if optimize:
-            _, self.base_r = self.optimize()
-        else:
-            self.base_r = None
+            _ = self.optimize()
         self.count = 1
 
 
@@ -201,42 +199,38 @@ class Program(object):
     def optimize(self):
         """
         Optimizes the constant tokens against the training data and returns the
-        optimized constants and base reward.
+        optimized constants.
 
         This function generates an objective function based on the training
         dataset, reward function, and constant optimizer. It ignores penalties
         because the Program structure is fixed, thus penalties are all the same.
         It then optimizes the constants of the program and returns the optimized
-        constants and base reward (reward without penalty).
+        constants.
 
         Returns
         _______
         optimized_constants : vector
             Array of optimized constants.
-
-        base_r : float
-            The base reward (reward without penalty) of the optimized program.
         """
 
         # Create the objective function, which is a function of the constants being optimized
         def f(consts):
             self.set_constants(consts)
             y_hat = self.execute(Program.X_train)
-            obj = -1*Program.reward_function(Program.y_train, y_hat)
+            obj = np.mean((Program.y_train - y_hat))
             return obj
         
         if len(self.const_pos) > 0:
             # Do the optimization
             x0 = np.ones(len(self.const_pos)) # Initial guess
-            optimized_constants, base_r = Program.const_optimizer(f, x0)
+            optimized_constants = Program.const_optimizer(f, x0)
             self.set_constants(optimized_constants)
 
         else:
             # No need to optimize if there are no constants
             optimized_constants = []
-            base_r = -f([])
 
-        return optimized_constants, base_r
+        return optimized_constants
 
 
     def set_constants(self, consts):
@@ -382,19 +376,20 @@ class Program(object):
         str_library = [f if isinstance(f, str) else f.name for f in Program.library]
         return np.array([str_library.index(f.lower()) for f in traversal], dtype=np.int32)
 
-    
-    def reward(self, X, y):
-        """Evaluates and returns the base reward under a given dataset"""
-
-        y_hat = self.execute(X)
-        return Program.reward_function(y, y_hat)
-
 
     @cached_property
     def complexity(self):
         """Evaluates and returns the complexity of the program"""
 
         return Program.complexity_penalty(self.traversal)
+
+
+    @cached_property
+    def base_r(self):
+        """Evaluates and returns the base reward of the program"""
+
+        y_hat = self.execute(Program.X_train)
+        return Program.reward_function(Program.y_train, y_hat)
 
 
     @cached_property

dsr/dsr/const.py
@@ -43,10 +43,6 @@ class ConstOptimizer(object):
         -------
         x : np.ndarray
             Vector of optimized constants.
-
-        r : float
-            Base reward (negative objective function) evaluated at optimized
-            constants.
         """
         raise NotImplementedError
 
@@ -59,9 +55,7 @@ class Dummy(ConstOptimizer):
 
     
     def __call__(self, f, x0):
-        x = x0
-        r = -f(x)
-        return x, r
+        return x0
         
 
 class ScipyMinimize(ConstOptimizer):
@@ -75,5 +69,4 @@ class ScipyMinimize(ConstOptimizer):
         with np.errstate(divide='ignore'):
             opt_result = partial(minimize, **self.kwargs)(f, x0)
         x = opt_result['x']
-        r = -opt_result['fun']
-        return x, r
+        return x

dsr/dsr/train.py
@@ -161,11 +161,9 @@ def learn(sess, controller, logdir=".", n_epochs=1000, batch_size=1000,
             programs = [from_tokens(a, optimize=False) for a in actions]
             programs_to_optimize = list(set([p for p in programs if p.base_r is None]))
             results = pool.map(work, programs_to_optimize)
-            for pair, p in zip(results, programs_to_optimize):
-                optimized_constants, base_r = pair
+            for optimized_constants, p in zip(results, programs_to_optimize):
                 p.set_constants(optimized_constants)
-                p.base_r = base_r
-        
+
         # Retrieve the rewards
         r = np.array([p.r for p in programs])
         base_r = np.array([p.base_r for p in programs])

commit e7fc0e83cda88f5449cab36a50737a63dcb2d13c

Fixed bug in which constant was optimizing mean (signed\!) error, not mean squared error.
This bug was introduced in 5bf4baf0e92, but was silent because most runs since then were not using constants.

dsr/dsr/program.py
@@ -209,7 +209,7 @@ class Program(object):
         def f(consts):
             self.set_constants(consts)
             y_hat = self.execute(Program.X_train)
-            obj = np.mean((Program.y_train - y_hat))
+            obj = np.mean((Program.y_train - y_hat)**2)
             return obj
         
         if len(self.const_pos) > 0:

dsr/dsr/run_benchmarks.py
@@ -31,7 +31,7 @@ def train_dsr(name_and_seed, config_dataset, config_controller, config_training)
     start = time.time()
 
     # Rename the output file
-    config_training["output_file"] = "dsr_{}_{}}.csv".format(name, seed)
+    config_training["output_file"] = "dsr_{}_{}.csv".format(name, seed)
 
     # Define the dataset and library
     dataset = get_dataset(name, config_dataset)