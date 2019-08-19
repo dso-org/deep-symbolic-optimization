7bfab07261e0000e1ab8467e39e65e2bc19c1033
Petersen, Brenden Kyle <petersen33@llnl.gov>
2019-08-19 12:26:42 -0700

    Updated ICLR config and experiments.

--- iclr/dsr_vs_gp/gp/run.sh
+++ iclr/dsr_vs_gp/gp/run.sh
@@ -1,1 +1,1 @@
-python -m dsr.run_benchmarks ../../iclr_config.json --method=deap --mc=100
+time python -m dsr.run_benchmarks ../../iclr_config.json --method=deap --only=Nguyen --only=Burks --only=Meier --mc=100
