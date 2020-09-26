a956ff040b5597f1ba982aad20e68ee4e9336b48
Petersen, Brenden Kyle <petersen33@llnl.gov>
2020-09-26 14:47:57 -0700

    Addressed PR tasks.

--- dsr/dsr/task/control/scripts/sample_zoo.py
+++ dsr/dsr/task/control/scripts/sample_zoo.py
@@ -1,32 +1,36 @@
 """Sampling obs, and action data from a Zoo policy on a Gym environment. Do, python sample_zoo.py --env ENV_NAME """
-import sys, os, gym
+import os
+import sys
+
 import numpy as np
-import dsr.task.control.utils as U
 import click
+import gym
+
+import dsr.task.control.utils as U
+
+DSP_DATA_ROOT = "../data"
+DSR_DATA_ROOT = "../../regression/data"
+REGRESSION_SEED_SHIFT = int(2e6)
+
 
 @click.command()
 @click.option("--env", type=str, default="LunarLanderContinuous-v2", help="Name of environment to sample")
-@click.option("--n_episodes", type=int, default=1000, help="Number of sampling episodes.")
-@click.option("--n_samples", type=int, default=100000, help="Number of sampling episodes.")
+@click.option("--n_episodes", type=int, default=1000, help="Number of episodes to sample.")
+@click.option("--n_samples", type=int, default=10000, help="Number of transitions to save.")
 def main(env,  n_episodes, n_samples):
-    #global variables
-    global REGRESSION_SEED_SHIFT 
-    global N_EPISODES
-    global SAMPLE_SIZE 
-    REGRESSION_SEED_SHIFT = int(2e6)
-    N_EPISODES = n_episodes
-    SAMPLE_SIZE = n_samples
+
     env_name = env
-    #Make gym environment
-    env=gym.make(env_name)
+
+    # Make gym environment
+    env = gym.make(env_name)
     if "Bullet" in env_name:
        env = U.TimeFeatureWrapper(env)
+
     #Load model
     U.load_default_model(env_name)
-    f = open(env_name+"_.csv", 'w')
-    #RUN episodes
-    for j in range(N_EPISODES):
-        env.seed(j + REGRESSION_SEED_SHIFT)
+
+    for i in range(n_episodes):
+        env.seed(i + REGRESSION_SEED_SHIFT)
         obs=env.reset()
         done = False
         obs_list = []
@@ -35,21 +39,26 @@ def main(env,  n_episodes, n_samples):
             obs_list.append(obs)
             action, _states = U.model.predict(obs)
             obs, rewards, done, info = env.step(action)
+            action = np.array(action)
             action_list.append(action)
-        # Save all action and obs as npz file
-        np.savez(env_name+"_action.npz", np.asarray(action_list))
-        np.savez(env_name+"_obs.npz", np.asarray(obs_list))
-        if len(obs_list)-1 >SAMPLE_SIZE : 
-            index = np.random.randint(0, len(obs_list)-1, SAMPLE_SIZE)
-            for i in index:
-                line=""
-                for s in obs_list[i]:
-                    line =  line + str(s) +","
-                for a in action_list[i]:
-                    line =  line + str(a)
-                print(j, line)
-                f.write(line+"\n")
-    f.close()
+
+    # Convert to array
+    # Columns correspond to [s1, s2, ..., sn, a1, a2, ..., an] for use by DSR
+    obs_array = np.array(obs_list)
+    action_array = np.array(action_list)
+    data = np.hstack([obs_array, action_array])
+
+    # Save all data as numpy file
+    path = os.path.join(DSP_DATA_ROOT, env_name + ".npz")
+    np.savez(path, data)
+
+    # Save randomly subset as CSV
+    np.random.seed(0) # For reproducibility
+    rows_to_keep = np.random.choice(data.shape[0], n_samples, replace=False)
+    data = data[rows_to_keep, :]
+    path = os.path.join(DSR_DATA_ROOT, env_name + ".csv")
+    np.savetxt(path, data, delimiter=",")
+
 
 if __name__ == "__main__":
     main()
