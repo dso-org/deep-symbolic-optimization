e5bcc93bc960c39ba35352579a8b649218b6fe5e
Pettit, Jacob F <pettit8@llnl.gov>
2020-10-19 16:29:57 -0700

    update tests and assertions to be more dynamic and to loop over dictionary of env names/kwargs

--- dsr/dsr/task/control/envs/test/test_lander.py
+++ dsr/dsr/task/control/envs/test/test_lander.py
@@ -5,77 +5,42 @@ import numpy as np
 from argparse import ArgumentParser
 
 def setup_envs_run_rollouts(seed: int = 0, rollout_length: int = 10):
-    gym_discrete_env = gym.make("LunarLander-v2")
-    gym_continuous_env = gym.make("LunarLanderContinuous-v2")
+    continuous_envs = {"LunarLanderContinuous-v2": {}, "LunarLanderCustom-v0": dict(reward_shaping_coef=1, continuous=True)}
+    discrete_envs = {"LunarLander-v2": {}, "LunarLanderCustom-v0": dict(reward_shaping_coef=1, continuous=False)}
+    all_envs = [discrete_envs, continuous_envs]
 
-    custom_discrete_env = gym.make("LunarLanderCustom-v0", reward_shaping_coef=1, continuous=False)
-    custom_continuous_env = gym.make("LunarLanderCustom-v0", reward_shaping_coef=1, continuous=True)
+    results = {}
+    for i, envs in enumerate(all_envs):
+        for env, kwargs in envs.items():
+            current_env = gym.make(env, **kwargs)
+            current_env.seed(seed)
+            current_env.action_space.seed(seed)
+            current_env.observation_space.seed(seed)
 
-    gym_discrete_env.seed(seed)
-    gym_continuous_env.seed(seed)
+            results[env] = {}
 
-    custom_discrete_env.seed(seed)
-    custom_continuous_env.seed(seed)
+            obs = current_env.reset()
+            r, done = 0, False
+            for i in range(rollout_length):
+                action = current_env.action_space.sample()
 
-    discrete_rollouts = []
+                results[env]["state"] = obs
+                results[env]["reward"] = r
+                results[env]["done"] = done
 
-    print("Entering discrete environments rollouts...")
-    # rollout discrete envs and record output
-    obs = gym_discrete_env.reset()
-    gym_r, gym_done = 0, False
-    custom_obs = custom_discrete_env.reset()
-    custom_r, custom_done = 0, False
-    for i in range(rollout_length):
-        action = gym_discrete_env.action_space.sample()
+                obs, r, done, _ = current_env.step(action)
 
-        this_step = ((obs, gym_r, gym_done), (custom_obs, custom_r, custom_done)) 
-        discrete_rollouts.append(this_step)
+            results[env]["state"] = obs
+            results[env]["reward"] = r
+            results[env]["done"] = done
 
-        obs, gym_r, gym_done, _ = gym_discrete_env.step(action)
-        custom_obs, custom_r, custom_done, _ = custom_discrete_env.step(action)
-
-    last_step = ((obs, gym_r, gym_done), (custom_obs, custom_r, custom_done)) 
-    discrete_rollouts.append(last_step)
-
-    print("Discrete rollouts finished.")
-
-
-    continuous_rollouts = []
-
-    print("Entering continuous environments rollouts...")
-    # rollout continuous envs and record output
-    obs = gym_continuous_env.reset()
-    gym_r, gym_done = 0, False
-    custom_obs = custom_continuous_env.reset()
-    custom_r, custom_done = 0, False
-    for i in range(rollout_length):
-        action = gym_continuous_env.action_space.sample()
-
-        this_step = ((obs, gym_r, gym_done), (custom_obs, custom_r, custom_done)) 
-        continuous_rollouts.append(this_step)
-
-        obs, gym_r, gym_done, _ = gym_continuous_env.step(action)
-        custom_obs, custom_r, custom_done, _ = custom_continuous_env.step(action)
-
-    last_step = ((obs, gym_r, gym_done), (custom_obs, custom_r, custom_done)) 
-    continuous_rollouts.append(last_step)
-
-    print("Continuous rollouts finished.")
-
-    print("Starting tests...")
-
-    for step in discrete_rollouts:
-        assert np.array_equal(step[0][0], step[1][0]), "States do not match."
-        assert step[0][1] == step[1][1], "Rewards obtained do not match."
-        assert step[0][2] == step[0][2], "Episode dones do not match."
-    print("Discrete tests passed.")
-
-    for step in continuous_rollouts:
-        assert np.array_equal(step[0][0], step[1][0]), "States do not match."
-        assert step[0][1] == step[1][1], "Rewards obtained do not match."
-        assert step[0][2] == step[0][2], "Episode dones do not match."
-    print("Continuous tests passed.")
-
+        names = list(envs.keys())
+        print(f"Comparing: {names}...")
+        assert np.array_equal(results[names[0]]['state'], results[names[1]]['state']), "States do not match between above environments."
+        assert np.array_equal(results[names[0]]['reward'], results[names[1]]['reward']), "Rewards do not match between above environments."
+        assert np.array_equal(results[names[0]]['done'], results[names[1]]['done']), "Episode dones do not match between above environments."
+        print(f"Tests passed.")
+    
     return 0
 
 if __name__ == '__main__':
