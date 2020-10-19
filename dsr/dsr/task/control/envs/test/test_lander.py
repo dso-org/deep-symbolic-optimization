65c52790d62f2e9256172dee575548e53af268ee
Pettit, Jacob F <pettit8@llnl.gov>
2020-10-19 16:34:08 -0700

    switch from using argparse to using click for parsing command line arguments

--- dsr/dsr/task/control/envs/test/test_lander.py
+++ dsr/dsr/task/control/envs/test/test_lander.py
@@ -2,9 +2,12 @@ import gym
 import dsr.task.control
 import numpy as np
 
-from argparse import ArgumentParser
+import click
 
-def setup_envs_run_rollouts(seed: int = 0, rollout_length: int = 10):
+@click.command()
+@click.option('--seed', default=0, help='Value for seeding environments.')
+@click.option('--rollout_length', default=10, help='How long of episodes to do rollouts for comparing environments.')
+def setup_envs_run_rollouts(seed: int, rollout_length: int):
     continuous_envs = {"LunarLanderContinuous-v2": {}, "LunarLanderCustom-v0": dict(reward_shaping_coef=1, continuous=True)}
     discrete_envs = {"LunarLander-v2": {}, "LunarLanderCustom-v0": dict(reward_shaping_coef=1, continuous=False)}
     all_envs = [discrete_envs, continuous_envs]
@@ -44,9 +47,4 @@ def setup_envs_run_rollouts(seed: int = 0, rollout_length: int = 10):
     return 0
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('-seed', '--seed', type=int, default=0)
-    parser.add_argument('-length', '--rollout_length', type=int, default=10)
-    args = parser.parse_args()
-
-    setup_envs_run_rollouts(args.seed, args.rollout_length)
+    setup_envs_run_rollouts()
