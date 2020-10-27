cc0a1b5a321e7b49b8f22654befe1d3339f1eb0e
glatt1 <glatt1@llnl.gov>
2020-10-27 01:21:33 -0700

    Improved flexibility with datasets and all config and source files

--- dsr/dsr/run.py
+++ dsr/dsr/run.py
@@ -166,7 +166,7 @@ def main(config_template, method, mc, output_filename, n_cores_task, seed_shift,
     # Required configs
     config_task = config["task"]            # Task specification parameters
     config_training = config["training"]    # Training hyperparameters
-    
+
     # Optional configs
     config_controller = config.get("controller")                        # Controller hyperparameters
     config_language_model_prior = config.get("language_model_prior")    # Language model hyperparameters
@@ -175,21 +175,25 @@ def main(config_template, method, mc, output_filename, n_cores_task, seed_shift,
     # Create output directories
     if output_filename is None:
         output_filename = "benchmark_{}.csv".format(method)
-    timestamp = datetime.now().strftime("%Y-%m-%d-%H%M%S")
-    config_training["logdir"] += "_" + timestamp
-    logdir = config_training["logdir"]
-    os.makedirs(logdir, exist_ok=True)
-    output_filename = os.path.join(logdir, output_filename)
-
+    config_training["logdir"] = os.path.join(
+        config_training["logdir"],
+        "log_{}".format(datetime.now().strftime("%Y-%m-%d-%H%M%S")))
+    if "backup" in config_task["dataset"] and config_task["dataset"]["backup"]:
+        config_task["dataset"]["logdir"] = config_training["logdir"]
+    os.makedirs(config_training["logdir"], exist_ok=True)
+    output_filename = os.path.join(config_training["logdir"], output_filename)
     # Use benchmark name from config if not specified as command-line arg
     if len(b) == 0:
-        b = (config_task["name"],)
+        if isinstance(config_task["name"], str):
+            b = (config_task["name"],)
+        elif isinstance(config_task["name"], list):
+            b = tuple(config_task["name"])
 
     # HACK: DSR-specific shortcut to run all Nguyen benchmarks
     benchmarks = list(b)
     if "Nguyen" in benchmarks:
         benchmarks.remove("Nguyen")
-        benchmarks += ["Nguyen-{}".format(i+1) for i in range(12)]    
+        benchmarks += ["Nguyen-{}".format(i+1) for i in range(12)]
 
     # Generate benchmark-seed pairs for each MC. When passed to the TF RNG,
     # seeds will be added to checksums on the benchmark names
@@ -214,10 +218,10 @@ def main(config_template, method, mc, output_filename, n_cores_task, seed_shift,
     print("Running {} for n={} on benchmarks {}".format(method, mc, unique_benchmarks))
 
     # Write terminal command and config.json into log directory
-    cmd_filename = os.path.join(logdir, "cmd.out")
+    cmd_filename = os.path.join(config_training["logdir"], "cmd.out")
     with open(cmd_filename, 'w') as f:
         print(" ".join(sys.argv), file=f)
-    config_filename = os.path.join(logdir, "config.json")
+    config_filename = os.path.join(config_training["logdir"], "config.json")
     with open(config_filename, 'w') as f:
         json.dump(config, f, indent=4)
 
@@ -225,7 +229,7 @@ def main(config_template, method, mc, output_filename, n_cores_task, seed_shift,
     if method == "dsr":
         work = partial(train_dsr, config_task=config_task, config_controller=config_controller, config_language_model_prior=config_language_model_prior, config_training=config_training)
     elif method == "gp":
-        work = partial(train_gp, logdir=logdir, config_task=config_task, config_gp=config_gp)
+        work = partial(train_gp, logdir=config_training["logdir"], config_task=config_task, config_gp=config_gp)
 
     # Farm out the work
     write_header = True
