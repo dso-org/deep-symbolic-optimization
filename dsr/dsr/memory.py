f1f0f9c25e13d28fbd6239f4c6ab2915a564cdf5
Petersen, Brenden Kyle <petersen33@llnl.gov>
2020-10-30 16:41:41 -0700

    Refactored priority queue classes.

---
+++ dsr/dsr/memory.py
@@ -0,0 +1,288 @@
+"""Classes for memory buffers, priority queues, and quantile estimation."""
+
+import heapq
+from typing import Tuple
+from collections import namedtuple
+from dataclasses import dataclass
+
+import numpy as np
+
+
+Batch = namedtuple(
+    "Batch", ["actions", "obs", "priors", "lengths", "rewards"])
+
+
+def get_samples(batch, key):
+    """
+    Returns a sub-Batch with samples from the given indices.
+
+    Parameters
+    ----------
+    key : int or slice
+        Indices of samples to return.
+
+    Returns
+    -------
+    batch : Batch
+        Sub-Batch with samples from the given indices.
+    """
+
+    batch = Batch(
+        actions=batch.actions[key],
+        obs=tuple(o[key] for o in batch.obs),
+        priors=batch.priors[key],
+        lengths=batch.lengths[key],
+        rewards=batch.rewards[key])
+    return batch
+
+# @dataclass
+# class Batch:
+#     """An iterable dataclass for a batch of samples."""
+
+#     actions: np.ndarray
+#     obs: Tuple[np.ndarray]
+#     priors: np.ndarray
+#     lengths: np.ndarray
+#     rewards: np.ndarray
+
+#     def __post_init__(self):
+#         L = self.actions.shape[0]
+#         assert len(self.obs) == 3, "Expected 3 observations."
+#         print([o.shape for o in self.obs])
+#         exit()
+#         assert L == self.priors.shape[0]
+#         assert L == self.lengths.shape[0]
+#         assert L == self.rewards.shape[0]
+
+#     def __getitem__(self, key):
+#         return Batch(
+#             actions=self.actions[key],
+#             obs=tuple(o[key] for o in self.obs),
+#             priors=self.priors[key],
+#             lengths=self.lengths[key],
+#             rewards=self.rewards[key])
+
+
+# Adapted from https://github.com/tensorflow/models/blob/1af55e018eebce03fb61bba9959a04672536107d/research/brain_coder/common/utils.py
+class ItemContainer(object):
+    """Class for holding an item with its score.
+
+    Defines a comparison function for use in the heap-queue.
+    """
+
+    def __init__(self, score, item, extra_data):
+        self.item = item
+        self.score = score
+        self.extra_data = extra_data
+
+    def __lt__(self, other):
+        assert isinstance(other, type(self))
+        return self.score < other.score
+
+    def __iter__(self):
+        """Allows unpacking like a tuple."""
+        yield self.score
+        yield self.item
+        yield self.extra_data
+
+    def __repr__(self):
+        """String representation of this item.
+
+        `extra_data` is not included in the representation. We are assuming that
+        `extra_data` is not easily interpreted by a human (if it was, it should be
+        hashable, like a string or tuple).
+
+        Returns:
+            String representation of `self`.
+        """
+        return str((self.score, self.item))
+
+    def __str__(self):
+        return repr(self)
+
+
+# Adapted from https://github.com/tensorflow/models/blob/1af55e018eebce03fb61bba9959a04672536107d/research/brain_coder/common/utils.py
+class UniquePriorityQueue(object):
+    """A priority queue where duplicates are not added.
+
+    The top items by score remain in the queue. When the capacity is reached,
+    the lowest scored item in the queue will be dropped.
+
+    This implementation differs from a typical priority queue, in that the minimum
+    score is popped, instead of the maximum. Largest scores remain stuck in the
+    queue. This is useful for accumulating the best known items from a population.
+
+    The items used to determine uniqueness must be hashable, but additional
+    non-hashable data may be stored with each item.
+    """
+
+    def __init__(self, capacity, seed=0):
+        self.capacity = capacity
+        self.heap = []
+        self.unique_items = set()
+        self.rng = np.random.RandomState(seed)
+
+    def push(self, score, item, extra_data=None):
+        """Push an item onto the queue.
+
+        If the queue is at capacity, the item with the smallest score will be
+        dropped. Note that it is assumed each item has exactly one score. The same
+        item with a different score will still be dropped.
+
+        Args:
+            score: Number used to prioritize items in the queue. Largest scores are
+                    kept in the queue.
+            item: A hashable item to be stored. Duplicates of this item will not be
+                    added to the queue.
+            extra_data: An extra (possible not hashable) data to store with the item.
+        """
+        if item in self.unique_items:
+            return
+        if len(self.heap) >= self.capacity:
+            _, popped_item, _ = heapq.heappushpop(
+                self.heap, ItemContainer(score, item, extra_data))
+            self.unique_items.add(item)
+            self.unique_items.remove(popped_item)
+        else:
+            heapq.heappush(self.heap, ItemContainer(score, item, extra_data))
+            self.unique_items.add(item)
+
+    def pop(self):
+        """Pop the item with the lowest score.
+
+        Returns:
+            score: Item's score.
+            item: The item that was popped.
+            extra_data: Any extra data stored with the item.
+        """
+        if not self.heap:
+            return ()
+        score, item, extra_data = heapq.heappop(self.heap)
+        self.unique_items.remove(item)
+        return score, item, extra_data
+
+    def get_max(self):
+        """Peek at the item with the highest score.
+
+        Returns:
+            Same as `pop`.
+        """
+        if not self.heap:
+            return ()
+        score, item, extra_data = heapq.nlargest(1, self.heap)[0]
+        return score, item, extra_data
+
+    def get_min(self):
+        """Peek at the item with the lowest score.
+
+        Returns:
+            Same as `pop`.
+        """
+        if not self.heap:
+            return ()
+        score, item, extra_data = heapq.nsmallest(1, self.heap)[0]
+        return score, item, extra_data
+
+    def random_sample(self, sample_size):
+        """Randomly select items from the queue.
+
+        This does not modify the queue.
+
+        Items are drawn from a uniform distribution, and not weighted by score.
+
+        Args:
+            sample_size: Number of random samples to draw. The same item can be
+                    sampled multiple times.
+
+        Returns:
+            List of sampled items (of length `sample_size`). Each element in the list
+            is a tuple: (item, extra_data).
+        """
+        idx = self.rng.choice(len(self.heap), sample_size, )
+        return [(self.heap[i].item, self.heap[i].extra_data) for i in idx]
+
+    def iter_in_order(self):
+        """Iterate over items in the queue from largest score to smallest.
+
+        Yields:
+            item: Hashable item.
+            extra_data: Extra data stored with the item.
+        """
+        for _, item, extra_data in heapq.nlargest(len(self.heap), self.heap):
+            yield item, extra_data
+
+    def __len__(self):
+        return len(self.heap)
+
+    def __iter__(self):
+        for _, item, _ in self.heap:
+            yield item
+
+    def __repr__(self):
+        return '[' + ', '.join(repr(c) for c in self.heap) + ']'
+
+    def __str__(self):
+        return repr(self)
+
+
+class UniquePriorityProgramQueue(UniquePriorityQueue):
+    """A UniquePriorityQueue with additional utilities specific to Batch
+    and Program."""
+
+    def push(self, sample, program):
+        """
+        Push a single sample corresponding to Program to the queue.
+
+        Parameters
+        ----------
+        sample : Batch
+            A Batch comprising a single sample.
+
+        program : Program
+            Program corresponding to the sample.
+        """
+
+        score = sample.rewards
+        id_ = program.str
+        super().push(score, id_, sample)
+
+    def push_batch(self, batch, programs):
+        """Push a Batch corresponding to Programs to the queue."""
+
+        for sample, program in zip(batch, programs):
+            self.push(sample, program)
+
+    def push_best(self, batch, programs):
+        """Push the single best sample from a Batch"""
+
+        i = np.argmax(batch.rewards)
+        sample = get_samples(batch, i)
+        program = programs[i]
+        self.push(sample, program)
+
+    def sample_batch(self, sample_size):
+        """Randomly select items from the queue and return them as a Batch."""
+
+        assert len(self.heap) > 0, "Cannot sample from an empty queue."
+        samples = [sample for (id_, sample) in self.random_sample(sample_size)]
+        batch = self.make_batch(samples)
+        return batch
+
+    def make_batch(self, samples):
+        """Turns a list of samples into a Batch."""
+
+        actions = np.stack([s.actions for s in samples], axis=0)
+        obs = tuple([np.stack([s.obs[i] for s in samples], axis=0) for i in range(3)])
+        priors = np.stack([s.priors for s in samples], axis=0)
+        lengths = np.array([s.lengths for s in samples], dtype=np.int32)
+        rewards = np.array([s.rewards for s in samples], dtype=np.float32)
+        batch = Batch(actions=actions, obs=obs, priors=priors,
+                      lengths=lengths, rewards=rewards)
+        return batch
+
+    def as_batch(self):
+        """Return the entire queue as a Batch."""
+
+        samples = [container.extra_data for container in self.heap]
+        batch = self.make_batch(samples)
+        return batch
