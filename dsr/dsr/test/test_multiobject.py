b1e9454be802a0353a8e9672b83cfd8a40a5576a
Pettit, Jacob F <pettit8@llnl.gov>
2021-01-08 15:57:51 -0800

    improve test for multiobject code, test n_objects=2 and n_objects=3. compare output of program execute to numpy output

--- dsr/dsr/test/test_multiobject.py
+++ dsr/dsr/test/test_multiobject.py
@@ -2,7 +2,7 @@ import pytest
 from pkg_resources import resource_filename
 
 from dsr.core import DeepSymbolicOptimizer
-from dsr.program import from_tokens, Program
+from dsr.program import from_tokens, from_str_tokens, Program
 from dsr.test.generate_test_data import CONFIG_TRAINING_OVERRIDE
 import tensorflow as tf
 import numpy as np
@@ -11,22 +11,17 @@ import numpy as np
 def model():
     return DeepSymbolicOptimizer("../config_test_multiobject.json")
 
-
-@pytest.fixture
-def cached_results(model):
-    save_path = resource_filename("dsr.test", "data/test_model")
-    model.load(save_path)
-    results = model.sess.run(tf.trainable_variables())
-
-    return results
-
-def test_model_parity(model, cached_results):
-    """Compare results to last"""
-
+def test_model(model):
     model.config_training.update(CONFIG_TRAINING_OVERRIDE)
-    model.train()
-    results = model.sess.run(tf.trainable_variables())
-
-    cached_results = np.concatenate([a.flatten() for a in cached_results])
-    results = np.concatenate([a.flatten() for a in results])
-    np.testing.assert_array_almost_equal(results, cached_results)
+    model.setup()
+    exec_nums = np.arange(0, 5, step=0.5)
+    exec_tokens = ["cos,x1,sin,x1", "exp,x1,cos,x1,sin,x1"]
+    np_fcns = [[lambda x: np.cos(x), lambda x: np.sin(x)], [lambda x: np.exp(x), lambda x: np.cos(x), lambda x: np.sin(x)]]
+    n_objs = [2, 3]
+    for i, str_tokens in enumerate(exec_tokens):
+        p = from_str_tokens(str_tokens, optimize=False, n_objects=n_objs[i])
+        funcs = np_fcns[i]
+        for n in exec_nums:
+            np_out = np.array([funcs[j](n) for j in range(len(funcs))]).squeeze()
+            prog_out = np.array(p.execute(np.array([[n]]))).squeeze()
+            np.testing.assert_array_almost_equal(np_out, prog_out)
