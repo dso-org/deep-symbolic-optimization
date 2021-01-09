2a7109ccb88656c54a1330165ec574763764fe6d
Pettit, Jacob F <pettit8@llnl.gov>
2021-01-08 16:10:03 -0800

    add comments to test multiobject file

--- dsr/dsr/test/test_multiobject.py
+++ dsr/dsr/test/test_multiobject.py
@@ -12,16 +12,18 @@ def model():
     return DeepSymbolicOptimizer("../config_test_multiobject.json")
 
 def test_model(model):
+    # update and setup model
     model.config_training.update(CONFIG_TRAINING_OVERRIDE)
     model.setup()
-    exec_nums = np.arange(0, 5, step=0.5)
-    exec_tokens = ["cos,x1,sin,x1", "exp,x1,cos,x1,sin,x1"]
-    np_fcns = [[lambda x: np.cos(x), lambda x: np.sin(x)], [lambda x: np.exp(x), lambda x: np.cos(x), lambda x: np.sin(x)]]
-    n_objs = [2, 3]
+
+    exec_nums = np.arange(0, 5, step=0.5) # values to test through execute and numpy functions
+    exec_tokens = ["cos,x1,sin,x1", "exp,x1,cos,x1,sin,x1"] # two different multi-object traversals to test
+    np_fcns = [[lambda x: np.cos(x), lambda x: np.sin(x)], [lambda x: np.exp(x), lambda x: np.cos(x), lambda x: np.sin(x)]] # numpy versions of the multi-object traversals
+    n_objs = [2, 3] # number of objects in each multi-object traversal
     for i, str_tokens in enumerate(exec_tokens):
-        p = from_str_tokens(str_tokens, optimize=False, n_objects=n_objs[i])
-        funcs = np_fcns[i]
-        for n in exec_nums:
-            np_out = np.array([funcs[j](n) for j in range(len(funcs))]).squeeze()
-            prog_out = np.array(p.execute(np.array([[n]]))).squeeze()
-            np.testing.assert_array_almost_equal(np_out, prog_out)
+        p = from_str_tokens(str_tokens, optimize=False, n_objects=n_objs[i]) # build program
+        funcs = np_fcns[i] # grab matching set of numpy functions
+        for n in exec_nums: # run execute and numpy functions over each test value
+            np_out = np.array([funcs[j](n) for j in range(len(funcs))]).squeeze() # keep output from numpy functions
+            prog_out = np.array(p.execute(np.array([[n]]))).squeeze() # keep output from program execute
+            np.testing.assert_array_almost_equal(np_out, prog_out) # assert outputs almost equal
