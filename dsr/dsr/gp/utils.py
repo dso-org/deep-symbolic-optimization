e27fb7f57e6a7ab023b34472eaf9fc99ce4fb82a
Petersen, Brenden Kyle <petersen33@llnl.gov>
2021-04-23 14:59:49 -0700

    Moved utility functions to utils.py

---
+++ dsr/dsr/gp/utils.py
@@ -0,0 +1,150 @@
+"""Genetic programming utils."""
+
+import random
+import copy
+from functools import wraps
+
+import numpy as np
+from deap import gp
+
+from dsr.program import _finish_tokens
+from dsr.subroutines import jit_parents_siblings_at_once
+
+
+def staticLimit(key, max_value):
+    """A fixed version of deap.gp.staticLimit that samples without replacement.
+    This prevents returning identical objects, for example if both children of a
+    crossover operation are illegal."""
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            # Copy args first in case func mutates them
+            keep_inds = [copy.deepcopy(ind) for ind in args]
+
+            new_inds = list(func(*args, **kwargs))
+            for i, ind in enumerate(new_inds):
+                if key(ind) > max_value:
+
+                    # Pop a random individual from keep_inds. This ensures we
+                    # don't sample the same object twice.
+                    pop_index = random.randint(0, len(keep_inds) - 1)
+                    new_inds[i] = keep_inds.pop(pop_index)
+
+            return new_inds
+        return wrapper
+    return decorator
+
+
+def multi_mutate(individual, expr, pset):
+    """Randomly select one of four types of mutation."""
+
+    v = np.random.randint(0, 4)
+
+    if v == 0:
+        individual = gp.mutUniform(individual, expr, pset)
+    elif v == 1:
+        individual = gp.mutNodeReplacement(individual, pset)
+    elif v == 2:
+        individual = gp.mutInsert(individual, pset)
+    elif v == 3:
+        individual = gp.mutShrink(individual)
+
+    return individual
+
+
+def create_primitive_set(lib):
+    """Create a DEAP primitive set from a dsr.libraryLibrary."""
+
+    pset = gp.PrimitiveSet("MAIN", len(lib.input_tokens))
+    rename_kwargs = {"ARG{}".format(i) : i for i in range(len(lib.input_tokens))}
+    for k, v in rename_kwargs.items():
+
+        # pset.renameArguments doesn't actually rename the Primitive.
+        # It just affects the pset mapping. So, rename the primitive here.
+        pset.mapping[k].name = v
+
+    pset.renameArguments(**rename_kwargs)
+
+    for i, token in enumerate(lib.tokens):
+        if token.input_var is None:
+            pset.addPrimitive(None, token.arity, name=i)
+
+    return pset
+
+
+def individual_to_dsr_aps(individual, library):
+    """Convert an individual to actions, parents, siblings."""
+
+    actions = np.array([[t.name for t in individual]], dtype=np.int32)
+    parent, sibling = jit_parents_siblings_at_once(actions, arities=library.arities, parent_adjust=library.parent_adjust)
+    return actions, parent, sibling
+
+
+def DEAP_to_tokens(individual):
+    """
+    Convert individual to tokens.
+
+    Parameters
+    ----------
+
+    individual : gp.PrimitiveTree
+        The DEAP individual.
+
+    Returns
+    -------
+
+    tokens : np.array
+        The tokens corresponding to the individual.
+    """
+
+    tokens = np.array([i.name for i in individual], dtype=np.int32)
+    return tokens
+
+
+def DEAP_to_padded_tokens(individual, max_length):
+    """
+    Convert individual to tokens padded to max_length.
+
+    Parameters
+    ----------
+
+    individual : gp.PrimitiveTree
+        The DEAP individual.
+
+    Returns
+    -------
+
+    tokens : np.array
+        The tokens corresponding to the individual.
+    """
+
+    actions = DEAP_to_tokens(individual) # Convert to unpadded actions
+    actions_padded = np.zeros(max_length, dtype=np.int32)
+    actions_padded[:len(actions)] = actions
+    return actions_padded
+
+
+def tokens_to_DEAP(tokens, pset):
+    """
+    Convert DSR tokens into DEAP individual.
+
+    Parameters
+    ----------
+    tokens : np.ndarray
+        Tokens corresponding to the individual.
+
+    pset : gp.PrimitiveSet
+        Primitive set upon which to build the individual.
+
+    Returns
+    _______
+    individual : gp.PrimitiveTree
+        The DEAP individual.
+    """
+
+    tokens = _finish_tokens(tokens)
+    plist = [pset.mapping[t] for t in tokens]
+    individual = gp.PrimitiveTree(plist)
+    return individual
