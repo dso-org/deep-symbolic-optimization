536580a8a81d69ef4c0d5de872dfc0aafeae045b
Petersen, Brenden Kyle <petersen33@llnl.gov>
2021-04-29 10:47:08 -0700

    Removed unused superclass.

--- dsr/dsr/gp/base.py
+++ dsr/dsr/gp/base.py
@@ -9,13 +9,24 @@ from deap import tools
 from dsr.program import from_tokens
 
 
-class GenericAlgorithm:
+class RunOneStepAlgorithm:
     """ Top level class which runs the GP, this replaces classes like eaSimple since we need 
         more control over how it runs.
     """
-    def __init__(self):
-        pass
+    def __init__(self, population, toolbox, cxpb, mutpb, stats=None, verbose=__debug__):
         
+        super(RunOneStepAlgorithm, self).__init__()
+        
+        self.logbook, self.population = self._header(population, toolbox, stats, verbose)
+        
+        self.toolbox        = toolbox
+        self.cxpb           = cxpb
+        self.mutpb          = mutpb
+        self.stats          = stats
+        self.verbose        = verbose
+        
+        self.gen        = 0
+
     def _eval(self, population, toolbox):
         
         # Evaluate the individuals with an invalid fitness
@@ -27,9 +38,9 @@ class GenericAlgorithm:
             actions = np.array(actions, dtype=np.int32)
             p = from_tokens(actions, optimize=True, n_objects=1, on_policy=False) # TBD: Support multi-objects
             ind.fitness.values = (-p.r,)
-            
+
         return population, invalid_ind
-            
+
     def _header(self, population, toolbox, stats=None,
                 verbose=__debug__):
         
@@ -66,60 +77,7 @@ class GenericAlgorithm:
                 del offspring[i].fitness.values
         
         return offspring
-    
-    def __call__(self, population, toolbox, cxpb, mutpb, ngen, stats=None,
-                 hof=None, verbose=__debug__):
-    
-        logbook, population = self._header(population, toolbox, stats, hof, verbose)
 
-        # TBD NEEDED HERE?
-        # Update hall of fame
-        if hof is not None:
-            hof.update(population)
-    
-        # Begin the generational process
-        for gen in range(1, ngen + 1):
-            
-            # Select the next generation individuals
-            offspring                           = toolbox.select(population, len(population))
-    
-            # Vary the pool of individuals
-            offspring                           = self._var_and(offspring, toolbox, cxpb, mutpb)
-    
-            # Evaluate the individuals with an invalid fitness
-            offspring, invalid_ind  = self._eval(offspring, toolbox)
-               
-            # Replace the current population by the offspring
-            population[:]                       = offspring
-    
-            # Append the current generation statistics to the logbook
-            record                              = stats.compile(population) if stats and population else {}
-            logbook.record(gen=gen, nevals=len(invalid_ind), **record)
-            
-            if verbose:
-                print(logbook.stream)
-    
-        return population, logbook
-    
-    
-class RunOneStepAlgorithm(GenericAlgorithm):
-    """ Top level class which runs the GP, this replaces classes like eaSimple since we need 
-        more control over how it runs.
-    """
-    def __init__(self, population, toolbox, cxpb, mutpb, stats=None, verbose=__debug__):
-        
-        super(RunOneStepAlgorithm, self).__init__()
-        
-        self.logbook, self.population = self._header(population, toolbox, stats, verbose)
-        
-        self.toolbox        = toolbox
-        self.cxpb           = cxpb
-        self.mutpb          = mutpb
-        self.stats          = stats
-        self.verbose        = verbose
-        
-        self.gen        = 0
-        
     def __call__(self, hof=None):
 
         t1                                          = time.perf_counter()
@@ -177,31 +135,3 @@ def create_stats_widget():
     mstats                  = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
     
     return mstats
-
-
-def generic_train(toolbox, hof, algorithm,
-                  population_size=1000, p_crossover=0.5, p_mutate=0.1, generations=1000,
-                  seed=0, verbose=True):
-    
-    """Train the GP"""
-
-    random.seed(seed)
-
-    pop         = toolbox.population(n=population_size)
-    
-    mstats      = create_stats_widget()
-    
-    pop, logbook = algorithm(population=pop,
-                             toolbox=toolbox,
-                             cxpb=p_crossover,
-                             mutpb=p_mutate,
-                             ngen=generations,
-                             stats=mstats,
-                             hof=hof,
-                             verbose=verbose)
-
-    # Delete custom classes
-    del creator.FitnessMin
-    del creator.Individual
-
-    return hof[0], logbook
