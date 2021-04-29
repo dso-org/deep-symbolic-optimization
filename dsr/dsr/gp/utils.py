dffe92b9b138876887495cc7a882070d328c0bd6
Petersen, Brenden Kyle <petersen33@llnl.gov>
2021-04-29 10:44:28 -0700

    Moved logbook pretty-printing to utils.

--- dsr/dsr/gp/utils.py
+++ dsr/dsr/gp/utils.py
@@ -3,12 +3,58 @@
 import random
 import copy
 from functools import wraps
+from collections import defaultdict
 
 import numpy as np
 from deap import gp
 
 from dsr.program import _finish_tokens
 from dsr.subroutines import jit_parents_siblings_at_once
+
+__type__ = object
+
+
+def cxOnePoint(ind1, ind2):
+    """Randomly select crossover point in each individual and exchange each
+    subtree with the point as root between each individual.
+    :param ind1: First tree participating in the crossover.
+    :param ind2: Second tree participating in the crossover.
+    :returns: A tuple of two trees.
+    """
+    if len(ind1) < 2 or len(ind2) < 2:
+        # No crossover on single node tree
+        return ind1, ind2
+
+    # List all available primitive types in each individual
+    types1 = defaultdict(list)
+    types2 = defaultdict(list)
+    if ind1.root.ret == __type__:
+        # Not STGP optimization
+        types1[__type__] = range(1, len(ind1))
+        types2[__type__] = range(1, len(ind2))
+        common_types = [__type__]
+    else:
+        for idx, node in enumerate(ind1[1:], 1):
+            types1[node.ret].append(idx)
+        common_types = []
+        for idx, node in enumerate(ind2[1:], 1):
+            if node.ret in types1 and not node.ret in types2:
+                common_types.append(node.ret)
+            types2[node.ret].append(idx)
+        # common_types = set(types1.keys()).intersection(set(types2.keys()))
+        # common_types = [x for x in types1 if x in types2]
+
+    if len(common_types) > 0:
+        type_ = random.choice(list(common_types))
+
+        index1 = random.choice(types1[type_])
+        index2 = random.choice(types2[type_])
+
+        slice1 = ind1.searchSubtree(index1)
+        slice2 = ind2.searchSubtree(index2)
+        ind1[slice1], ind2[slice2] = ind2[slice2], ind1[slice1]
+
+    return ind1, ind2
 
 
 def staticLimit(key, max_value):
@@ -148,3 +194,82 @@ def tokens_to_DEAP(tokens, pset):
     plist = [pset.mapping[t] for t in tokens]
     individual = gp.PrimitiveTree(plist)
     return individual
+
+
+def str_logbook(logbook, header_only=False, startindex=0):
+    """
+    Pretty print a deap.tools.Logbook. 
+    """
+    
+    if header_only:
+        startindex  = 0
+        endindex    = 1
+    else:
+        endindex    = -1
+    
+    columns = logbook.header
+    
+    if not columns:
+        columns = sorted(logbook[0].keys()) + sorted(logbook.chapters.keys())
+                            
+    if not logbook.columns_len or len(logbook.columns_len) != len(columns):
+        logbook.columns_len = map(len, columns)
+
+    # Start index is set at function call, or is 0 if doing the header
+
+    chapters_txt = {}
+    offsets = defaultdict(int)
+    for name, chapter in logbook.chapters.items():
+        chapters_txt[name] = chapter.__txt__(startindex)
+        if startindex == 0:
+            offsets[name] = len(chapters_txt[name]) - len(logbook)
+
+    str_matrix = []
+    
+    for i, line in enumerate(logbook[startindex:endindex]):
+        str_line = []
+        for j, name in enumerate(columns):
+            if name in chapters_txt:
+                # Put Chapter over the column label line
+                column = chapters_txt[name][i+offsets[name]]
+            else:
+                # Put the column label
+                value = line.get(name, "")
+                string = "{0:n}" if isinstance(value, float) else "{0}"
+                column = string.format(value)
+            logbook.columns_len[j] = max(logbook.columns_len[j], len(column))
+            str_line.append(column)
+        str_matrix.append(str_line)
+                
+    if startindex == 0 and logbook.log_header:
+        header = []
+        nlines = 1
+        if len(logbook.chapters) > 0:
+            nlines += max(map(len, chapters_txt.values())) - len(logbook) + 1
+        header = [[] for i in range(nlines)]
+        
+        for j, name in enumerate(columns):
+            if name in chapters_txt:
+                length = max(len(line.expandtabs()) for line in chapters_txt[name])
+                blanks = nlines - 2 - offsets[name]
+                for i in range(blanks):
+                    header[i].append(" " * length)
+                header[blanks].append(name.center(length))
+                header[blanks+1].append("-" * length)
+                for i in range(offsets[name]):
+                    header[blanks+2+i].append(chapters_txt[name][i])
+            else:
+                length = max(len(line[j].expandtabs()) for line in str_matrix)
+                for line in header[:-1]:
+                    line.append(" " * length)
+                header[-1].append(name)
+        
+        if header_only:
+            str_matrix = header  
+        else:
+            str_matrix = chain(header, str_matrix)
+        
+    template    = "\t".join("{%i:<%i}" % (i, l) for i, l in enumerate(logbook.columns_len))
+    text        = [template.format(*line) for line in str_matrix]
+    
+    return "\n".join(text)
