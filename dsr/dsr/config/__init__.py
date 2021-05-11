4902952e0d5dbd1bfbe8b0bc82fb320f1f19fac9
glatt1 <glatt1@llnl.gov>
2021-05-11 07:08:13 -0700

    Prepared configs for task specific base configs

--- dsr/dsr/config/__init__.py
+++ dsr/dsr/config/__init__.py
@@ -1,7 +1,19 @@
 import json
 import os
 
-def get_base_config():
+from dsr.utils import safe_merge_dicts
+
+def get_base_config(task):
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "base_config.json"), encoding='utf-8') as f:
         base_config = json.load(f)
-    return base_config
+
+    # Load task specific config
+    if task in ["regression", "control"]:
+        task_config_file = "config_dsr.json"
+    elif task in ["gp"]:
+        task_config_file = "config_gp.json"
+
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), task_config_file), encoding='utf-8') as f:
+        task_config = json.load(f)
+
+    return safe_merge_dicts(base_config, task_config)
