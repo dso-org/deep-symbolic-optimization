fed84ef7c7f1c48a55b75b6f8aafb55cc2df729b
glatt1 <glatt1@llnl.gov>
2021-05-11 07:23:52 -0700

    Small improvements

--- dsr/dsr/config/__init__.py
+++ dsr/dsr/config/__init__.py
@@ -4,15 +4,18 @@ import os
 from dsr.utils import safe_merge_dicts
 
 def get_base_config(task):
+    # Load base config
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "base_config.json"), encoding='utf-8') as f:
         base_config = json.load(f)
 
     # Load task specific config
+    task_config_file = None
     if task in ["regression", "control"]:
         task_config_file = "config_dsr.json"
     elif task in ["gp"]:
         task_config_file = "config_gp.json"
-
+    else:
+        assert False, "*** ERROR: Unknown task type: {}".format(task)
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), task_config_file), encoding='utf-8') as f:
         task_config = json.load(f)
 
