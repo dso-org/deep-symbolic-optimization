18ba611f0dfbabc14cb88a35097e58c0eba8cb7f
Pettit <pettit8@Ohana02.llnl.gov>
2021-06-28 16:32:59 -0700

    switch to HardCodedConstant, pull Brendens suggested change

--- dsr/dsr/test/test_constant.py
+++ dsr/dsr/test/test_constant.py
@@ -1,8 +1,8 @@
-from dsr.library import Constant
+from dsr.library import HardCodedConstant
 import numpy as np
 
 def test_constant():
     valid_cases = np.arange(0, 25, 0.1)
     for number in valid_cases:
-        const = Constant(value=number)
-        assert const.function() == number, "Value returned from Constant.function() ({}) does not match input value ({}).".format(const.function(), number)
+        const = HardCodedConstant(value=number)
+        assert const() == number, "Value returned from Constant.function() ({}) does not match input value ({}).".format(const(), number)
