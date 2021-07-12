dee7d207e10cd0d1b2fb8989f8ea0b61cf667be4
glatt1 <glatt1@llnl.gov>
2021-07-12 12:04:56 -0700

    Changed in paper run scripts

--- paper/run_Nguyen_dsr.sh
+++ paper/run_Nguyen_dsr.sh
@@ -1,1 +1,1 @@
-time python -m dso.run config/base_1M_new.json --method=dso --b=Nguyen... --mc=100 --n_cores_task=32
+time python -m dso.run config/base_1M_new.json --method=dso --b=Nguyen... --runs=100 --n_cores_task=32
